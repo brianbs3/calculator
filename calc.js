99c4bc203db41f9c270a2ef1e762abd7636eb87a
Brian Sizemore <bsizemore@BSMBPro.local>
2020-05-05 18:54:08 -0400

    add median function

--- calc.js
+++ calc.js
@@ -1,4 +1,4 @@
-let nums = [1,3,4,5,0,8];
+let nums = [77,12345,4,0,8,7];
 const _ = require('lodash');
 // console.log(numbers);
 // let newNumbers = numbers.map(n => n * 2);
@@ -28,10 +28,27 @@ const mean = (numbers) => {
     return(sum(numbers) / numbers.length);
 }
 
+/**
+ * return the list of numbers sorted by number value
+ * @param {*} numbers 
+ */
+const sort = (numbers) => {
+    return numbers.sort((a,b) =>{
+        return a - b;
+    });
+}
+
+const median = (numbers) => {
+    numbers = sort(numbers);
+    return (numbers.length % 2 === 0) ? mean([numbers[parseInt(numbers.length/2) - 1], numbers[parseInt(numbers.length/2)]]) : numbers[parseInt(numbers.length/2)];
+}
+
 if(_.isArray(nums)){
     console.log('numbers:', nums);
+    console.log('sorted:', sort(nums));
     console.log('double:', double_each(nums));
     console.log('square:', square_each(nums));
     console.log('sum:', sum(nums));
     console.log('mean:', mean(nums));
+    console.log('median:', median(nums));
 }
