c6a3384e2cf1fabca7a193fcf917249d61ba9d77
Brian Sizemore <bsizemore@BSMBPro.local>
2020-05-06 22:03:49 -0400

    lodash

--- js/routes/calc.js
+++ js/routes/calc.js
@@ -1,20 +1,25 @@
 'use strict';
 
 const router = require('express').Router();
+const _ = require('lodash');
 const {double_each,square_each,sort,mean,median} = require('../calc_functions');
 
 router.post('/all', async(req, res) =>{
     try{
         const n = req.body.numbers;
         console.log(req.body);
-        return res.json({
-            original_numbers: n,
-            sorted_numbers: sort(n),
-            double_each: double_each(n),
-            square_each: square_each(n),
-            mean: mean(n),
-            median: median(n)
-        });
+        if(_.isArray(n)){
+            return res.json({
+                original_numbers: n,
+                sorted_numbers: sort(n),
+                double_each: double_each(n),
+                square_each: square_each(n),
+                mean: mean(n),
+                median: median(n)
+            });
+        }
+        else
+            return res.status(401).json({ status: 'FAILED', message: 'seems its not an array' });
     }
     catch(err){
         console.log('error', err);
